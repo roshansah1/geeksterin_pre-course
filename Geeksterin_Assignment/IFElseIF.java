afdcc291b2c463fd5d4f5e503831e8ea2985effb
roshansah1 <Roshansah9909@gmail.com>
2022-04-06 02:04:45 +0530

    program to find greatest among three number

---
+++ Geeksterin_Assignment/IFElseIF.java
@@ -0,0 +1,23 @@
+package April_05_2022;
+
+public class IFElseIF {
+
+	public static void main(String[] args) {
+		
+		int num1 = 100;
+		int num2 = 300;
+		int num3 = 900;
+		
+		if (num1>num2 && num1>num3)
+			System.out.println(num1);
+		
+			else if (num2>num1 && num2>num3)
+				System.out.println(num2);
+		
+			else 
+				System.out.println(num3);
+		
+		
+	}
+
+}

f935a2b9b972cefe65058a3186074c96e30dfbf6
Roshan sah <34867634+roshansah1@users.noreply.github.com>
2022-04-06 23:55:23 +0530

    using scanner

--- Geeksterin_Assignment/Elseif.java
+++ Geeksterin_Assignment/Elseif.java
@@ -22,7 +22,7 @@ public class Elseif {
 				System.out.println(num2 + " is the greatest number");
 		
 			else 
-				System.out.println(num3 + " is the greatest number");
+			 System.out.println(num3 + " is the greatest number");
 			
 	}
 

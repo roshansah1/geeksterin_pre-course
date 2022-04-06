97ef7f781c7c2b912c486aa0aff2a4c36f366baa
roshansah1 <Roshansah9909@gmail.com>
2022-04-07 01:20:13 +0530

    Assignment_2

---
+++ Geeksterin_Assignment/Prime.java
@@ -0,0 +1,27 @@
+package April_06_2022;
+
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		
+		int num;
+		int count=0;
+		System.out.println("Enter any number");
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+		 
+			if ( num % i == 0) {
+				count++;
+			}
+		}
+		if(count==2) 
+			System.out.println("prime number");
+		else
+			System.out.println("Not prime number");	
+	}
+
+}
